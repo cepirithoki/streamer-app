5c438f535e513e40ed64e9659327840eef7b4a06
cepirithoki <cepirithoki@gmail.com>
2025-03-17 12:18:24 +0700

    Create gradlew.bat

---
+++ gradlew.bat
@@ -0,0 +1,8 @@
+@if "%DEBUG%" == "" @echo off
+@rem ##########################################################################
+@rem
+@rem  Gradle startup script for Windows
+@rem
+@rem ##########################################################################
+
+# ... [Full content here: https://raw.githubusercontent.com/gradle/gradle/master/gradlew.bat] ...
